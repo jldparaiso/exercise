139096c04bee6493d3efba3cfa716ac7d42fc92e
jldparaiso <jldparaiso@gmail.com>
2019-03-18 05:15:04 +0800

    exercise

---
+++ js/init.js
@@ -0,0 +1,19 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+    $('.collapsible').collapsible();
+
+    $('.fixed-action-btn').floatingActionButton();
+
+    $('.tooltipped').tooltip();
+
+    $('.slider').slider();
+
+    $('.carousel').carousel();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
